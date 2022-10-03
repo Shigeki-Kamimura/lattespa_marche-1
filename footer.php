a417e1b77e9cd3afcb0caf00d8ba17b9b52e275d
ozawaaoi <blue.aoringo7@gmail.com>
2022-10-03 19:20:35 +0900

    フッターの作成

---
+++ footer.php
@@ -0,0 +1,35 @@
+<footer>
+    <div>
+        <div>運営会社情報</div>
+        <div class="latee_box">
+            <div>
+                <img src="lattepla_logo.png" alt="Latte planning total beauty produce">
+                <div>株式会社ラテプランニング</div>
+            </div>
+            <div>
+                <div>〒101-0023</div>
+                <div>東京都千代田区神田松永町７ヤマリビル５階</div>
+                <div>TEL:03-6206-0043</div>
+                <div>FAX:03-6206-0046</div>
+                <div>営業時間AM10:00-PM5:00</div>
+            </div>
+        </div>
+        <img src="kakeru.png" alt="×">
+        <div class="nr_box">
+            <div>
+                <img src="nr_logo.png" alt="NEXT RESERVATION株式会社">
+                <div>NEXT RESERVATION株式会社</div>
+            </div>
+            <div>
+                 <div> 〒368-1211</div>
+                 <div> 長野県上田市下之郷812-1</div>
+                 <div> TEL:0268-71-7315</div>
+                 <div>営業時間AM9:00-PM6:00</div>
+            </div>
+    </div> 
+    <div>
+         <div>Copyright(c) 2022 Latee Planning Co.,Ltd. ALL Rights Reserved.</div>
+    </div>
+</footer>
+
+            
