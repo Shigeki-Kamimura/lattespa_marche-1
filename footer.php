0cded49f1b540bc9572e6b6c27beeb2d70e5fdda
ozawaaoi <blue.aoringo7@gmail.com>
2022-10-10 18:31:49 +0900

    横並び

--- footer.php
+++ footer.php
@@ -13,25 +13,31 @@
     <div>
         <div>運営会社情報</div>
         <div class="latee_box">
-            <div class="foot_1">
-                <img src="<?= $base ?>lattepla_logo.png" alt="Latte planning total beauty produce"class="foot_logo">
+            <div class="boxContainer"> 
+            <div class="box"> 
+            <div class="foot_1"> 
+                <img src="<?= $base ?>lattepla_logo.png" alt="Latte planning total beauty produce">
                 <div>株式会社ラテプランニング</div>
+            </div>   
             </div>
-            <adress class="foot_about">
-                <div>〒101-0023</div>
-                <div>東京都千代田区神田松永町７ヤマリビル５階</div>
+            <div class="box"> 
+                <div >〒101-0023</div>
+                <div >東京都千代田区神田松永町７ヤマリビル５階</div>
                 <div>TEL:03-6206-0043</div>
                 <div>FAX:03-6206-0046</div>
                 <div>営業時間AM10:00-PM5:00</div>
-            </adress>  
+            </div>  
         </div>
         <img src="kakeru.png" alt="×">
         <div class="nr_box">
-            <div>
-                <img src="nr_logo.png" alt="NEXT RESERVATION株式会社">
+            <div class="boxContainer">
+            <div class="box_1">
+            <div class="foot_2">
+                <img src="<?= $base ?>nr_logo.png" alt="NEXT RESERVATION株式会社">
                 <div>NEXT RESERVATION株式会社</div>
             </div>
-            <div>
+            </div>
+            <div class="box_1">
                  <div> 〒368-1211</div>
                  <div> 長野県上田市下之郷812-1</div>
                  <div> TEL:0268-71-7315</div>
