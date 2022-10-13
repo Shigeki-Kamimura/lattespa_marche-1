ba8d9e42d724a68edc2ee75afcfc5e3099f5dca1
chisa614 <105782604+chisa614@users.noreply.github.com>
2022-10-13 18:05:38 +0900

    ラテスパフッター作成

--- footer.php
+++ footer.php
@@ -1,46 +1,42 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="initial-scale=1.0">
-<link rel="stylesheet" href="style-ozawa.css">
-</head>
-<body>
-<?php include_once('footer.php');?>
-</body>
-</html>
 <footer>
-    <div>
-        <div>運営会社情報</div>
-        <div class="latee_box">
-            <div class="foot_1">
-                <img src="<?= $base ?>lattepla_logo.png" alt="Latte planning total beauty produce"class="foot_logo">
-                <div>株式会社ラテプランニング</div>
+    <section class="basic_block">
+        <div class="bg_beige">
+            <h1>運営会社情報</h1>
+            <div class="flexbox">
+                <div class="company_name">
+                    <div class="img_wrapper">
+                        <img src="<?= $base ?>lattepla_logo.png" alt="Latte planning total beauty produce">
+                    </div>
+                    <div>株式会社ラテプランニング</div>
+                </div>
+                <address>
+                    <div>〒101-0023</div>
+                    <div>東京都千代田区神田松永町７ヤマリビル５階</div>
+                    <a href="tel:03-6206-0043">TEL:03-6206-0043</a>
+                    <a href="fax:03-6206-0046">FAX:03-6206-0046</a>
+                    <div>営業時間AM10:00-PM5:00</div>
+                </address>  
             </div>
-            <adress class="foot_about">
-                <div>〒101-0023</div>
-                <div>東京都千代田区神田松永町７ヤマリビル５階</div>
-                <div>TEL:03-6206-0043</div>
-                <div>FAX:03-6206-0046</div>
-                <div>営業時間AM10:00-PM5:00</div>
-            </adress>  
+            <div class="img_wrapper">
+                <img src="<?= $base ?>kakeru.png" alt="×" class="kakeru">
+            </div>
+            <div class="flexbox">
+                <div class="company_name">
+                    <div class="img_wrapper">
+                        <img src="<?= $base ?>nr_logo.png" alt="NEXT RESERVATION株式会社">
+                    </div>
+                    <div>NEXT RESERVATION株式会社</div>
+                </div>
+                <address>
+                    <div> 〒368-1211</div>
+                    <div> 長野県上田市下之郷812-1</div>
+                    <a href="tel:0268-71-7315">TEL:0268-71-7315</a>
+                    <div>営業時間:AM9:00-PM6:00</div>
+                </address>
+            </div>
         </div>
-        <img src="kakeru.png" alt="×">
-        <div class="nr_box">
-            <div>
-                <img src="nr_logo.png" alt="NEXT RESERVATION株式会社">
-                <div>NEXT RESERVATION株式会社</div>
-            </div>
-            <div>
-                 <div> 〒368-1211</div>
-                 <div> 長野県上田市下之郷812-1</div>
-                 <div> TEL:0268-71-7315</div>
-                 <div>営業時間AM9:00-PM6:00</div>
-            </div>
-    </div> 
-    <div>
-         <div class="Copyright">2022 Latee Planning Co.,Ltd. ALL Rights Reserved.</div>
+    </section>
+    <div class="footer">
+        <div class="copyright">&copy; 2022 Latee Planning Co.,Ltd. ALL Rights Reserved.</div>
     </div>
-</footer>
-
-            
+</footer>    
